a0751c0fbc89ed3c7c0f89dee91a3fe6e1ed057f
andela-nmartin <godmetweenciati@gmail.com>
2015-12-14 12:33:01 +0300

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,1 @@
+angular.module('budgetApp', ['budgetApp.controllers']);
---
+++ app/scripts/controllers.js
@@ -0,0 +1,14 @@
+angular.module('budgetApp.controllers', [])
+  .controller('FinanceController', function($scope) {
+    $scope.salary = 0;
+    $scope.rent = 17500;
+    $scope.electricity = 1200;
+    $scope.phone = 1500;
+    $scope.cic = 3000;
+    $scope.transport = 3000;
+    $scope.food = 5000;
+
+    $scope.result = function() {
+      return $scope.salary - (0.1 * $scope.salary) - $scope.rent - $scope.transport - $scope.food - $scope.electricity - $scope.phone - $scope.cic;
+    };
+  });
